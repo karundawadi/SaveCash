4ced4fa56bb23e76ce21543f4f6797851034d1be
Karun Dawadi <karun.dawadi@mavs.uta.edu>
2021-07-20 17:06:37 -0500

    Datepicker added.
    Date picker is added and working.

--- component_views/addexpense.tsx
+++ component_views/addexpense.tsx
@@ -1,23 +1,47 @@
 import React from 'react';
-import {StyleSheet, View, Text, TextInput, Alert, Image, TouchableHighlight} from 'react-native';
+import {StyleSheet, View, Text, Button, TextInput, Platform ,Alert, Image, TouchableHighlight} from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
 function AddExpense({ navigation } : {navigation : any}){
     const [expenseName, onExpenseNameChange] = React.useState("")
+    const [date, setDate] = React.useState(new Date(1598051730000));
+
+    const onChange = (event:any, selectedDate:any) => {
+        const currentDate = selectedDate || date;
+        setDate(currentDate);
+    };
+
     return (
         <View style={styles.base}>
             <View style={styles.form}>
                 {/* Expense Name */}
                 <Text style={styles.textForm}>Your monthlty income</Text>
+
                 <View style={{flexDirection:"column", paddingTop:10, paddingLeft:10,paddingRight:10}}>
                     <TextInput style={styles.inputField} value={expenseName} onChangeText={onExpenseNameChange} placeholder="Enter your expense" keyboardType="default"></TextInput>
                 </View>
-                
+
+                <View style={{padding:'2%'}}></View>
+
                 {/* Expense Name */}
                 <Text style={styles.textForm}>Your monthlty income</Text>
                 <View style={{flexDirection:"column", paddingTop:10, paddingLeft:10,paddingRight:10}}>
                     <TextInput style={styles.inputField} value={expenseName} onChangeText={onExpenseNameChange} placeholder="Enter your expense" keyboardType="default"></TextInput>
                 </View>
+
+                <View style={{padding:'2%'}}></View>
+                
+                <View style={{flexDirection:'row', alignItems:'flex-start',paddingLeft:10}}>
+                    <Text style={{flex:2}}>Date</Text>
+                    <DateTimePicker style={{flex:2}}
+                        testID="dateTimePicker"
+                        value={date}
+                        display="default"
+                        onChange={onChange}
+                    />
+                </View>
+                
+                <View style={{padding:'2%'}}></View>
 
                 {/* Expense Name */}
                 <Text style={styles.textForm}>Your monthlty income</Text>
